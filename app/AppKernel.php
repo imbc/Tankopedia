2e507e1584271206634aa7358d77e80bc34d19db
touristtam <tourist.tam@gmail.com>
2013-04-13 10:44:05 +0100

    moved debug bar extra

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,8 +29,8 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new Elao\WebProfilerExtraBundle\WebProfilerExtraBundle();
             $bundles[] = new Lsw\VersionInformationBundle\LswVersionInformationBundle();
+            $bundles[] = new Elao\WebProfilerExtraBundle\WebProfilerExtraBundle();
         }
 
         return $bundles;
