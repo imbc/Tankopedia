84b759c2d111e12fd2eb2df60a23b4a6925b7cc1
touristtam <tourist.tam@gmail.com>
2014-03-19 21:56:27 +0000

    didnt understood the search ajax feature .... wip

--- src/Imbc/SearchEngineBundle/Controller/SearchController.php
+++ src/Imbc/SearchEngineBundle/Controller/SearchController.php
@@ -4,6 +4,7 @@ namespace Imbc\SearchEngineBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Description of SearchController
@@ -12,9 +13,18 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class SearchController extends Controller
 {
-
-    public function indexAction()
+    public function autocompleteAction( Request $request )
     {
-        return new Response( 'Hello world!' );
+        $em = $this->getDoctrine()->getEntityManager();
+        $tankTable = $em->getClassMetadata( 'ImbcTankopediaBundle:Tank' )->getTableName();
+        $term = $request->get( 'term' );
+        $connection = $this->get( 'database_connection' );
+        $dql = "SELECT t.id, t.name as value
+                FROM " . $tankTable . " t
+                WHERE t.name LIKE ?
+                ORDER BY t.name ASC";
+        $entities = $connection->fetchAll( $dql, array( $term . '%' ));
+
+        return new Response( json_encode( $entities ));
     }
 }
