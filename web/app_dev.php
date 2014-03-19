d6a0fb286eef5f5ce45598f43a33ecc66217460b
thoroc <tourist.tam@gmail.com>
2014-03-19 09:35:17 +0000

    chickenshit

--- web/app_dev.php
+++ web/app_dev.php
@@ -15,12 +15,12 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
-
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
+//echo 'bar';
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
